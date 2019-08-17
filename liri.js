c28a4d3ca77ae4a769f8ee02859f789443d60943
Kalamath <thecodedimage@gmail.com>
2019-08-17 00:43:30 -0700

    done for the night

---
+++ liri.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+// Basic Node application for requesting data from the OMDB website via axios
+// Here we incorporate the "axios" npm package
+var axios = require("axios");
+
+var DigitalAssistant = function () {
+    // this.hungry = false;
+    // this.sleepy = false;
+    // this.bored = true;
+    // this.age = 0;
+
+    this.movie = function () {
+        // We then run the request with axios module on a URL with a JSON
+        axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                // Then we print out the imdbRating
+                console.log("Title: " + response.data.Title);
+                console.log("Release Year: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Score: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Cast: " + response.data.Actors);
+            }
+        );
+
+    };
+}
+
+var bots = {};
+
+bots.Liri = new DigitalAssistant();
+
+// var spotify = new Spotify(keys.spotify);
+
+var bot = process.argv[2];
+var method = process.argv[3];
+
+bots[bot][method]();
