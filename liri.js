72065c4e6d4406d662c239dce67b74c5447acf9a
Kalamath <thecodedimage@gmail.com>
2019-08-18 21:43:30 -0700

    Got concert and Spotify to pull information

--- liri.js
+++ liri.js
@@ -5,9 +5,10 @@ var keys = require("./keys.js");
 // Here we incorporate the "axios" npm package
 var axios = require("axios");
 var moment = require("moment");
+var Spotify = require('node-spotify-api');
 
 var DigitalAssistant = function () {
-   
+
     this.movie = function () {
         // We then run the request with axios module on a URL with a JSON
         axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
@@ -26,32 +27,52 @@ var DigitalAssistant = function () {
                 // handle error
                 console.log(error);
             })
-            .finally(function() {
+            .finally(function () {
                 // always executed
             })
 
     };
 
-    this.concert = function() {
+    this.concert = function () {
         axios.get("https://rest.bandsintown.com/artists/tyler+the+creator/events?app_id=codingbootcamp").then(
             function (response) {
                 // handle success
+                console.log("==============================================================");
                 console.log("Venue: " + response.data[0].venue.name);
                 console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
                 console.log("Date: " + JSON.stringify({
-                    datetime : moment().format("L")}));
+                    datetime: moment().format("L")
+                }));
+                console.log("==============================================================");
             })
             .catch(function (error) {
                 // handle error
                 console.log(error);
             })
-            .finally(function() {
+            .finally(function () {
                 // always executed
             })
     };
 
-    this.spotify = function() {
+    this.spotify = function () {
         console.log("How about we add some Spotify too!");
+        // Variable for Spotify id & secret
+        var spotify = new Spotify({
+            id: "632c7bb33dfd44c0bd974aad84faec95",
+            secret: "08529520af314327b98ed30d791bf984"
+        });
+
+        spotify
+            .search({ type: 'track', query: 'pyramids' })
+            .then(function (response) {
+                console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                console.log("Track: " + response.tracks.items[0].name);
+                console.log("Album: " + response.tracks.items[0].album.name);
+                console.log("Preview Link: " + response.tracks.items[0].external_urls.spotify);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     };
 }
 
